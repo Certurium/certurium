9e1ca92fe92a47a497d61b09abee990a1fd63b43
Peter Bushnell <bushsolo@gmail.com>
2018-10-11 09:33:22 +0100

    NeoScrypt

--- src/util/system.h
+++ src/util/system.h
@@ -40,6 +40,7 @@
 // Application startup time (used for uptime calculation)
 int64_t GetStartupTime();
 
+extern unsigned int nNeoScryptOptions;
 extern const char * const BITCOIN_CONF_FILENAME;
 
 void SetupEnvironment();
