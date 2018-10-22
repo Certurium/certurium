dd8d8ecb3b0719d23dacdfee71d9652b79bafde0
Peter Bushnell <bushsolo@gmail.com>
2018-10-22 16:23:53 +0000

    Skip AcceptPendingSyncCheckpoint on IBD

--- src/validation.cpp
+++ src/validation.cpp
@@ -3847,7 +3847,8 @@ bool CChainState::AcceptBlock(const std::shared_ptr<const CBlock>& pblock, Block
 
     CheckBlockIndex(chainparams.GetConsensus());
 
-    AcceptPendingSyncCheckpoint();
+    if (!IsInitialBlockDownload())
+        AcceptPendingSyncCheckpoint();
 
     return true;
 }
