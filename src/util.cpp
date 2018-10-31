f483d4be72a33f71857eb823a7aa6abc5e698d21
Jack London <jack@london.co.uk>
2018-10-31 06:19:53 +0100

    Chainparams

--- src/util.cpp
+++ src/util.cpp
@@ -710,7 +710,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Platinumcoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -720,10 +720,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Platinumcoin";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".platinumcoin";
 #endif
 #endif
 }
