525b14c3c2fbba958c8f707d40156cb4d0a7a3b0
Jack London <jack@london.co.uk>
2018-11-05 11:32:07 +0100

    grincoin's T3 difficulty adjustment algorithm
    The difficulty adjustment algorithm has been taken as in
    commit 7754adb834fda6ecc14bdd747562593e67b3925e.
    Additionally, the time median window has been removed
    as in 0e413e97928b1e8b80ceaa1e3dbfb0c057904ef0 (only visible
    in PR 1765) and the cleanup from commit
    e1b5d7f3c22b417fc560558dd188326a29074768 has been applied.

--- src/pow.cpp
+++ src/pow.cpp
@@ -5,70 +5,73 @@
 
 #include <pow.h>
 
-#include <arith_uint256.h>
 #include <chain.h>
 #include <primitives/block.h>
 #include <uint256.h>
 
+static const int64_t DIFFICULTY_ADJUST_WINDOW = 60;
+
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
-    assert(pindexLast != nullptr);
-    unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
+    if (params.fPowAllowMinDifficultyBlocks)
+        return UintToArith256(params.powLimit).GetCompact();
 
-    // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
-    {
-        if (params.fPowAllowMinDifficultyBlocks)
-        {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
-        return pindexLast->nBits;
+    assert(pindexLast != nullptr);
+    const auto BitsToArith256 = [](uint32_t bits){
+        arith_uint256 value;
+        value.SetCompact(bits);
+        return value;
+    };
+
+    const size_t needed_block_count = DIFFICULTY_ADJUST_WINDOW;
+    std::vector<std::pair<int64_t, arith_uint256>> past_data;
+    past_data.reserve(needed_block_count);
+    for (auto p = pindexLast; p; p = p->pprev) {
+        past_data.emplace_back(p->GetBlockTime(), BitsToArith256(p->nBits));
+        if (past_data.size() == needed_block_count)
+            return CalculateNextWorkRequired(past_data, params);
     }
 
-    // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval()-1);
-    assert(nHeightFirst >= 0);
-    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
-    assert(pindexFirst);
-
-    return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
+    const auto last_ts_delta = past_data.size() > 2 ?
+        past_data.front().first - past_data.at(1).first :
+        params.nPowTargetSpacing;
+    auto last_ts = past_data.size() > 1 ?
+        std::next(past_data.crbegin())->first - last_ts_delta :
+        past_data.front().first;
+    const auto last_diff = past_data.front().second;
+    // fill in simulated blocks with values from the previous real block
+    for (size_t i = past_data.size(); i < needed_block_count; i++) {
+        last_ts = last_ts - last_ts_delta;
+        past_data.emplace_back(last_ts, last_diff);
+    }
+    assert(past_data.size() == needed_block_count);
+    return CalculateNextWorkRequired(past_data, params);
 }
 
-unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
+unsigned int CalculateNextWorkRequired(const std::vector<std::pair<int64_t, arith_uint256>>& past_data, const Consensus::Params& params)
 {
     if (params.fPowNoRetargeting)
-        return pindexLast->nBits;
+        return past_data.front().second.GetCompact();
 
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-    if (nActualTimespan < params.nPowTargetTimespan/4)
-        nActualTimespan = params.nPowTargetTimespan/4;
-    if (nActualTimespan > params.nPowTargetTimespan*4)
-        nActualTimespan = params.nPowTargetTimespan*4;
 
-    // Retarget
+    const int64_t DAMP_FACTOR = 3;
+    const auto BLOCK_TIME_WINDOW = (DIFFICULTY_ADJUST_WINDOW - 1) * params.nPowTargetSpacing;
+    const auto UPPER_TIME_BOUND = BLOCK_TIME_WINDOW * 2;
+    const auto LOWER_TIME_BOUND = BLOCK_TIME_WINDOW / 2;
+
     const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    arith_uint256 bnNew;
-    bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
+    arith_uint256 targetAvg;
+    for (const auto& data: past_data)
+        targetAvg += data.second;
+    targetAvg /= DIFFICULTY_ADJUST_WINDOW;
+    const auto ts_delta = past_data.front().first - past_data.back().first;
+    const auto ts_damp = DAMP_FACTOR * targetAvg > bnPowLimit ?
+        ts_delta :
+        (ts_delta + (DAMP_FACTOR - 1) * BLOCK_TIME_WINDOW) / DAMP_FACTOR;
 
-    if (bnNew > bnPowLimit)
-        bnNew = bnPowLimit;
-
-    return bnNew.GetCompact();
+    const auto adj_ts = std::max(std::min(ts_damp, UPPER_TIME_BOUND), LOWER_TIME_BOUND);
+    const arith_uint256 target = targetAvg * adj_ts / BLOCK_TIME_WINDOW;
+    return target > bnPowLimit ? bnPowLimit.GetCompact() : target.GetCompact();
 }
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
